cbc1860a9be0b3096f5ea703ce17f5d54fc85b4c
OlafurTorfi <olafurtorfi@gmail.com>
2016-01-04 06:20:15 +0900

    Just added a bunch of missing semi-colons

--- generators/app/templates/_server.js
+++ generators/app/templates/_server.js
@@ -1,9 +1,9 @@
-var express = require('express')
+var express = require('express');
 
-var app = express()
+var app = express();
 
-app.use('/node_modules', express.static(__dirname + '/node_modules'))
-app.use('/', express.static(__dirname + '/src'))
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
+app.use('/', express.static(__dirname + '/src'));
 
-app.listen(3000)
-console.log('Listening on port 3000.')
+app.listen(3000);
+console.log('Listening on port 3000.');
--- generators/app/templates/_server_livereload.js
+++ generators/app/templates/_server_livereload.js
@@ -1,17 +1,17 @@
-var express = require('express')
-var livereload = require('livereload')
+var express = require('express');
+var livereload = require('livereload');
 
-var app = express()
+var app = express();
 
-app.use('/node_modules', express.static(__dirname + '/node_modules'))
-app.use('/', express.static(__dirname + '/src'))
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
+app.use('/', express.static(__dirname + '/src'));
 
-app.listen(3000)
-console.log('Listening on port 3000.')
+app.listen(3000);
+console.log('Listening on port 3000.');
 
 app = livereload.createServer({
   exts: ['html', 'css', 'js', 'png', 'gif', 'jpg', 'ts']
-})
+});
 
-app.watch(__dirname + '/src')
-console.log('Live reload enabled')
+app.watch(__dirname + '/src');
+console.log('Live reload enabled');
